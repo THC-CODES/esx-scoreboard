3c3765c9053b1ed2c50b5ffe3eb5780e373aecd8
THC <88347509+THCVP@users.noreply.github.com>
2021-12-03 00:14:03 -0800

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+games  'gta5' 
+author 'Stadus'
+
+--Forked By 'THC'
+
+client_scripts {
+	'client/main.lua'
+}
+
+server_scripts {
+	'server/main.lua'
+}
+
+ui_page {
+	'html/scoreboard.html'
+}
+
+files {
+	'html/scoreboard.html',
+	'html/style.css',
+	'html/listener.js'
+}
